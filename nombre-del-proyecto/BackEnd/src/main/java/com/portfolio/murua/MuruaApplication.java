4cd1ed636ed91d5d56baed6542252ec0477db587
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-05-12 19:09:01 -0300

    Backend
    Springboot Backend

---
+++ nombre-del-proyecto/BackEnd/src/main/java/com/portfolio/murua/MuruaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.murua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuruaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuruaApplication.class, args);
+	}
+
+}
---
+++ nombre-del-proyecto/BackEnd/src/test/java/com/portfolio/murua/MuruaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.murua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuruaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
