83801042af2dc61e83a01113876351d115f5ae2a
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-05-29 16:40:30 -0300

    adapt
    adapt

--- nombre-del-proyecto/FrontEnd/src/app/componentes/header/header.component.ts
+++ nombre-del-proyecto/FrontEnd/src/app/componentes/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  colors:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.colors=data.colors;
+    })
   }
 
 }
